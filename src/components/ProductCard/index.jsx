f20f8d707c634229274842a896aaae0168a752b7
Nataliia Vorobei <natalia.vorobei27@gmail.com>
2024-09-06 11:07:04 +0300

    added deleting, editpage

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
 import styles from './ProductCard.module.scss';
-// import { baseUrl } from '../../utils/vars';
+import img from './product-image.webp';
+import { useDispatch } from 'react-redux';
+import { deleteProductAsync } from '../../redux/actions/productsActions';
 
-export default function ProductCard({id, imageUrl, title, price}){
+export default function ProductCard({id, title}){
+    const dispatch = useDispatch();
     return(
         <div className={styles.productCard}>
+            <div className={styles.productCard__actions}>
+                <button className={styles.productCard__actions_edit}>✎</button>
+                <button onClick={dispatch(deleteProductAsync())} className={styles.productCard__actions_del}>🗑</button>
+            </div>
             <Link to={`/products/${id}`} className={styles.productCard__link}>
-                {/* <img className={styles.productCard__img} src={imageUrl[0]} alt={title} width={'100px'} /> */}
-                <h3 className={styles.productCard__title}>{title}</h3>
+                <img className={styles.productCard__img} src={img} alt={title} />
+                <h3 className={styles.productCard__title}>{title.split(" ").slice(0, 3).join(" ") + "..."}</h3>
             </Link>
-            <p className={styles.productCard__price}>${price}</p>
+            <p className={styles.productCard__price}>Price: $60</p>
         </div>
     )
 }
